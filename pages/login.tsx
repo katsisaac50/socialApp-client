be33bd2c80214485d2e0d265cf47dbcd741138a2
katsisaac50 <katsisaac50@gmail.com>
2023-11-14 02:58:33 +0100

    created login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,13 @@
-const login = () => {
+import React from 'react';
+
+const LoginPage = () => {
     return (
         <div className='col'>
-                <div className='row'>
-                        <h1 className='display-1 text-center py-5'>Login Page</h1>
-                </div>
+            <div className='row'>
+                <h1 className='display-1 text-center py-5'>Login Page</h1>
+            </div>
         </div>
     );
 }
+
+export default LoginPage;
