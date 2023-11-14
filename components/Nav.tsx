57d7205957e0c3a7b434d937c0f2bca6914cb72a
katsisaac50 <katsisaac50@gmail.com>
2023-11-14 03:02:46 +0100

    created Register page

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -13,9 +13,9 @@ function NavBar(props: NavBarProps) {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
---
+++ pages/register.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Register = () => {
+  // Your component logic goes here
+
+  return (
+    <div>
+      <div className='col'>
+            <div className='row'>
+                <h1 className='display-1 text-center py-5'>Register Page</h1>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Register;
