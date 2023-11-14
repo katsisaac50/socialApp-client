e0b3a50d7888cc4a6955ecf8af66b494863d4793
katsisaac50 <katsisaac50@gmail.com>
2023-11-14 23:43:31 +0100

    static resoureces updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,10 @@ function HomePage() {
     <div className='col'>
         <div className='row'>
             <h1 className='display-1 text-center py-5'>Home Page</h1>
-            <img className='mx-auto d-block d-md-none' src='https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' />
-            <img className='mx-auto d-none d-md-block' src='.../public/images/default.jpeg' /> 
+            <img className='mx-auto d-none d-md-block' src='/images/default.jpeg' /> 
+            <img className='mx-auto d-none d-md-block' src='https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' />
+            <img className='mx-auto d-none d-md-block' src='https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' />
+            
         </div>
     </div>
   );
