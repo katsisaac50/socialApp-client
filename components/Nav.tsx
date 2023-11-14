1bad6c85bf0f24a5d3a401af82c21203fa299e06
katsisaac50 <katsisaac50@gmail.com>
2023-11-14 02:47:59 +0100

    navbar created

---
+++ components/Nav.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+function NavBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">My Website</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@
 
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from'../components/Nav';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
+      <Navbar bg="black" expand="lg"/>
       <Component {...pageProps} />
     </div>
   );
