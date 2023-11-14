30622c1b348019e68a4633a1c65789e739e8d3c9
katsisaac50 <katsisaac50@gmail.com>
2023-11-14 07:14:30 +0100

    add public folder for static resources

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,8 @@ function HomePage() {
     <div className='col'>
         <div className='row'>
             <h1 className='display-1 text-center py-5'>Home Page</h1>
+            <img className='mx-auto d-block d-md-none' src='https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' />
+            <img className='mx-auto d-none d-md-block' src='.../public/images/default.jpeg' /> 
         </div>
     </div>
   );
