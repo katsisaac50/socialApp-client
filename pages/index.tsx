b5919ef8f7d84ea5937edeadff8d2ab01c5c9556
katsisaac50 <katsisaac50@gmail.com>
2023-11-15 07:02:24 +0100

    added more features on registration page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,11 @@ function HomePage() {
   return (
     <div className='col'>
         <div className='row'>
-            <h1 className='display-1 text-center py-5'>Home Page</h1>
-            <img className='mx-auto d-none d-md-block' src='/images/default.jpeg' alt='image'/> 
-            <img className='mx-auto d-none d-md-block' src='https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' />
-            <img className='mx-auto d-none d-md-block' src='https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' />
-            
-        </div>
+            <h1 className='display-1 text-center py-5 '>Home Page</h1>
+            <img className='mx-auto d-none d-md-block w-50 py-5' src='/images/default.jpeg' alt='image'/> 
+            <img className='mx-auto d-none d-md-block w-50 py-5' src='https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' />
+            <img className='mx-auto d-none d-md-block w-50 py-5' src='https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' />         
+        </div>className='mx-auto d-none d-md-block'
     </div>
   );
 }
