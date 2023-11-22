cebd4cec15e51c4ff91667a473028f796ce71f2e
katsisaac50 <katsisaac50@gmail.com>
2023-11-22 08:42:00 +0100

    format and cleanup

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import axios from "axios";
-import {toast} from 'react-toastify';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 import { Modal, Button } from 'antd';
 import Link from 'next/link';
 import AuthForm from '../components/forms/Authform';
@@ -16,8 +16,6 @@ const Register = () => {
   const [consent, setConsent] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  
-
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -29,36 +27,41 @@ const Register = () => {
       password,
       repeatPassword,
     };
-  
+
     try {
       setLoading(true);
       const response = await axios.post(
         `${process.env.NEXT_PUBLIC_API}/register`,
-        postData, {
+        postData,
+        {
           headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          }
-      }
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        }
       );
-      setName('');
-      setEmail('');
-      setSelectedQuestion('');
-      setSecretAnswer('');
-      setPassword('');
-      setRepeatPassword('');
-      setLoading(false);
       toast.success(response.data.message, {
         theme: 'colored',
-      })
+      });
       setSuccess(response.data.success);
-      // console.log(response.data);
     } catch (error) {
       toast.error(error.response.data.message, {
         theme: 'colored',
       });
       console.error('Error making request:', error);
+    } finally {
       setLoading(false);
+      // Reset form fields
+      resetForm();
     }
+  };
+
+  const resetForm = () => {
+    setName('');
+    setEmail('');
+    setSelectedQuestion('');
+    setSecretAnswer('');
+    setPassword('');
+    setRepeatPassword('');
   };
 
   const handleSuccessClose = () => {
@@ -66,73 +69,85 @@ const Register = () => {
   };
 
   return (
-    <div className='container-fluid p-0'>
-      <div className='row py-5 bg-default-image text-light'>
-        <div className='col text-center '>
+    <div className="container-fluid p-0">
+      <div className="row py-5 bg-default-image text-light">
+        <div className="col text-center">
           <h1 className="display-3">Register Page</h1>
         </div>
       </div>
-      <section className="vh-200 bg-image"
-            style={{ backgroundImage: "url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp')" }}>
-            <div className="mask d-flex align-items-center h-100 gradient-custom-3">
-              <div className="container h-100">
-                <div className="row d-flex justify-content-center align-items-center h-100">
-                  <div className="col-12 col-md-9 col-lg-7 col-xl-6">
-                    <div className="card mt-5 mb-5" style={{ borderRadius: "15px" }}>
-                      <div className="card-body p-5">
-                        <h2 className="text-uppercase text-center mb-5">Create an account</h2>
-      
-      <AuthForm 
-      handleSubmit={handleSubmit}
-      name={name}
-      setName={setName}
-      email={email}
-      setEmail={setEmail}
-      selectedQuestion={selectedQuestion}
-      setSelectedQuestion={setSelectedQuestion}
-      secretAnswer={secretAnswer}
-      setSecretAnswer={setSecretAnswer}
-      password={password}
-      setPassword={setPassword}
-      repeatPassword={repeatPassword}
-      setRepeatPassword={setRepeatPassword}
-      consent={consent}
-      setConsent={setConsent}
-      loading={loading}
-      />
-      <div className="row">
+      <section
+        className="vh-200 bg-image"
+        style={{
+          backgroundImage:
+            "url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp')",
+        }}
+      >
+        <div className="mask d-flex align-items-center h-100 gradient-custom-3">
+          <div className="container h-100">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+              <div className="col-12 col-md-9 col-lg-7 col-xl-6">
+                <div
+                  className="card mt-5 mb-5"
+                  style={{ borderRadius: '15px' }}
+                >
+                  <div className="card-body p-5">
+                    <h2 className="text-uppercase text-center mb-5">
+                      Create an account
+                    </h2>
+
+                    <AuthForm
+                      handleSubmit={handleSubmit}
+                      name={name}
+                      setName={setName}
+                      email={email}
+                      setEmail={setEmail}
+                      selectedQuestion={selectedQuestion}
+                      setSelectedQuestion={setSelectedQuestion}
+                      secretAnswer={secretAnswer}
+                      setSecretAnswer={setSecretAnswer}
+                      password={password}
+                      setPassword={setPassword}
+                      repeatPassword={repeatPassword}
+                      setRepeatPassword={setRepeatPassword}
+                      consent={consent}
+                      setConsent={setConsent}
+                      loading={loading}
+                    />
+                    <div className="row">
                       <div className="col">
-                      <p className="text-center text-muted mt-5 mb-0">
-    Have already an account? <a href="/login" className="fw-bold text-body"><u>Login here</u></a>
-  </p>
+                        <p className="text-center text-muted mt-5 mb-0">
+                          Have already an account?{' '}
+                          <Link href="/login" passHref>
+  
+                              <u>Login here</u>
+                            
+                          </Link>
+                        </p>
                       </div>
                     </div>
-      </div>
-                    </div>
-                    
                   </div>
                 </div>
               </div>
             </div>
-          </section>
-          <div className="row">
-            <div className="col">
-            <Modal
-      title="Success"
-      visible={success}
-      onCancel={handleSuccessClose}
-      footer={[
-        <Link href="/login" key="login">
-          <Button>
-            Go to Login
-          </Button>
-        </Link>
-      ]}
-    >
-      <p>Account created successfully. Please login.</p>
-    </Modal>
-            </div>
           </div>
+        </div>
+      </section>
+      <div className="row">
+        <div className="col">
+          <Modal
+            title="Success"
+            visible={success}
+            onCancel={handleSuccessClose}
+            footer={[
+              <Link href="/login" key="login">
+                <Button>Go to Login</Button>
+              </Link>,
+            ]}
+          >
+            <p>Account created successfully. Please login.</p>
+          </Modal>
+        </div>
+      </div>
     </div>
   );
 };
