434edbcaedd4d2e15e6c2960e2f8d0b135f725c7
katsisaac50 <katsisaac50@gmail.com>
2023-11-28 23:55:25 +0100

    implement intercepter to clear local storage when token is expired

--- context/index.js
+++ context/index.js
@@ -1,4 +1,5 @@
 import { useState, createContext, useEffect} from 'react';
+import axios from 'axios';
 
 const UserContext = createContext();
 
@@ -10,7 +11,26 @@ const UserProvider = ({ children }) => {
 
     useEffect(() => {
         setState(JSON.parse(window.localStorage.getItem('auth')));
-    }, [])
+    }, []);
+
+    axios.interceptors.response.use((response) => {
+
+        // if (response.status === 401) {
+        //     setState({ ...state, user: null, token: '' });
+        //     window.localStorage.removeItem('auth');
+        // }
+
+        return response;
+        
+    }, (error) => {
+        let res = error.response;
+        if(res.status === 401 && res.config && !res.config.__isRetryRequest){
+            setState(null);
+            window.localStorage.removeItem('auth');
+            Router.push('/login');
+        }
+    });
+
     return (
         <UserContext.Provider value={[ state, setState ]}>
             {children}
