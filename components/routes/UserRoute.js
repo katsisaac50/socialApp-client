5603e2cf86135440d44a20cb9ad45bcbb9b8e08a
katsisaac50 <katsisaac50@gmail.com>
2023-11-28 04:46:50 +0100

    clean up done

--- components/routes/UserRoute.js
+++ components/routes/UserRoute.js
@@ -1,33 +1,34 @@
-import React, {useEffect, useState} from'react';
-import {useRouter} from 'next/router';
-import axios from 'axios';  
-import {SyncOutlined} from '@ant-design/icons';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import { SyncOutlined } from '@ant-design/icons';
 
-function UserRoute({children}) {
-    const [ok, setOk] = useState(false);
-    const router = useRouter();
-    useEffect(() => {
-        fetchUser();
-    }, []);
-}
+function UserRoute({ children }) {
+  const [ok, setOk] = useState(false);
+  const router = useRouter();
 
-async function fetchUser() {
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  async function fetchUser() {
     try {
-        const {data} = await axios.get(
-            `${process.env.NEXT_PUBLIC_API}/current-user`
-            );
-        if (data.ok) setOk(true);
+      const { data } = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/current-user`
+      );
+      if (data.ok) setOk(true);
     } catch (err) {
-        console.log(err);
-        setOk(false);
-        router.push('/login');
-    };
+      console.log(err);
+      setOk(false);
+      router.push('/login');
+    }
+  }
 
-    return !ok ? (
-        <SyncOutlined spin className="d-flex justify-content-center display-1 text-primary p-5"/>
-    ):(
-        {children}
-    )
+  return !ok ? (
+    <SyncOutlined spin className="d-flex justify-content-center display-1 text-primary p-5" />
+  ) : (
+    <>{children}</>
+  );
 }
 
 export default UserRoute;
