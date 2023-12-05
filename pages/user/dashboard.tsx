050f3abad48a09375db7ae0747bbe896d0a8ef6c
katsisaac50 <katsisaac50@gmail.com>
2023-12-05 03:09:56 +0100

    use axios in postSubmit function to save the posts in database

--- pages/user/dashboard.tsx
+++ pages/user/dashboard.tsx
@@ -4,13 +4,31 @@ import { useRouter } from "next/router";
 import { UserContext } from "../../context";
 import UserRoute from "../../components/routes/UserRoute";
 import CreatePostForm from '../../components/forms/CreatePostForm';
-
+import axios from "axios";
 
 const Dashboard = () => {
 
     const [state, setState] = useContext(UserContext);
     const [content, setContent] = useState("");
     const router = useRouter();
+
+    const handleQuillChange = (value) => {
+        // Remove <p> tags from the HTML content
+        const contentWithoutPTags = value.replace(/<\/?p>/gi, '');
+    
+        // Set the modified content to state
+        setContent(contentWithoutPTags);
+      };
+
+      const postSubmit = async () => {
+        try {
+          const response = await axios.post(`${process.env.NEXT_PUBLIC_API}/create-post`, {content});
+          console.log(response);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+
 console.log(content);
     if (state === null) {
         router.push('/login');
@@ -25,7 +43,7 @@ console.log(content);
             <h2>Hello {existingUser && existingUser.name}</h2>
             <CreatePostForm 
                 content = {content}
-                setContent = {setContent}
+                handleQuillChange = {handleQuillChange}
                 postSubmit={() => {}}
             />
         </div>
