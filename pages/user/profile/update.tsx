e65b60265e27a1d08627ebe7506900ee4dabc0c4
katsisaac50 <katsisaac50@gmail.com>
2023-12-27 10:19:22 +0100

    implement user profile image

--- pages/user/profile/update.tsx
+++ pages/user/profile/update.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
-import { Modal, Button } from 'antd';
+import { Modal, Button, Avatar } from 'antd';
 import Link from 'next/link';
 import AuthForm from '../../../components/forms/Authform';
 import { UserContext } from '../../../context';
 import { useRouter } from 'next/router';
+import { LoadingOutlined, CameraOutlined } from '@ant-design/icons';
 
 const ProfileUpdate = () => {
     const [username, setUsername] = useState("");
@@ -21,6 +22,8 @@ const ProfileUpdate = () => {
   const [loading, setLoading] = useState(false);
   const [state, setState] = useContext(UserContext);
   const router = useRouter();
+  const [image, setImage] = useState({});
+  const [uploading, setUploading] = useState(false);
 
   useEffect(()=>{
   // console.log(state.user)
@@ -71,7 +74,7 @@ const ProfileUpdate = () => {
         theme: 'colored',
       });
       setSuccess(data.success);
-      
+
       setLoading(false);
 
     } catch (error) {
@@ -89,6 +92,45 @@ const ProfileUpdate = () => {
   const handleSuccessClose = () => {
     setSuccess(false);
   };
+
+  const handleImageUpload = async (e) => {
+    e.preventDefault();
+    
+    const file = e.target.files[0];
+
+    const formData = new FormData();
+    formData.append('image', file);
+
+    setUploading(true);
+
+    try {
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_API}/upload-image`, formData);
+
+      setUploading(false);
+
+      setImage({
+        url: response.data.result.secure_url,
+        public_id: response.data.result.public_id
+      });
+
+      if(response.data.success) {
+        toast.success(response.data.message, {
+          theme: 'colored',
+        });
+      } else {
+        toast.error(response.data.message, {
+          theme: 'colored',
+        });
+      }
+
+    } catch (err) {
+
+      console.log(err);
+      setUploading(false);
+
+    }
+    
+  }
 
 //   if(state && state.token) {
 //     router.push('/');
@@ -120,7 +162,18 @@ const ProfileUpdate = () => {
                     <h2 className="text-uppercase text-center mb-5">
                       Update account
                     </h2>
-
+                    <label className="form-label d-flex justify-content-center h5">
+                    {image&&image.url ? (
+                    <Avatar size={30} src={image.url} />
+                    ) : uploading ? (
+                        <LoadingOutlined className="text-primary me-2 mb-1" style={{display: uploading ? "inline-block" : "none"}}/> 
+                        ): (
+                        <Avatar size={30} icon={<CameraOutlined />} />
+                        )}
+                    {/* <LoadingOutlined className="text-primary me-2 mb-1" style={{display: uploading ? "inline-block" : "none"}}/> */}
+                    {/* <CameraOutlined className="text-primary me-2 mb-1"/> */}
+                    <input  onChange={handleImageUpload} type="file" accept="image/*" hidden/>
+                </label>
                     <AuthForm
                       handleSubmit={handleSubmit}
                       name={name}
