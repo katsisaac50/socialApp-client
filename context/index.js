bc5f74d44ddad5f8f2752bdd81e484ec919fec36
katsisaac50 <katsisaac50@gmail.com>
2024-02-18 09:50:44 +0100

    get post on home page

--- context/index.js
+++ context/index.js
@@ -16,31 +16,25 @@ const UserProvider = ({ children }) => {
         setState(prevState => ({ ...prevState, ...JSON.parse(window.localStorage.getItem('auth')) }));
 
         // Cleanup interceptors
-    return () => {
-        // Remove the interceptor when the component is unmounted
-        axios.interceptors.response.eject(myInterceptor);
-
-    };
-    
+        return () => {
+            // Remove the interceptor when the component is unmounted
+            axios.interceptors.response.eject(myInterceptor);
+        };
     }, []);
 
     const token = state && state.token ? state.token : '';
+
+    // console.log('Token:', token);
 
     axios.defaults.baseURL = process.env.NEXT_PUBLIC_API;
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
     axios.interceptors.response.use((response) => {
-
-        // if (response.status === 401) {
-        //     setState({ ...state, user: null, token: '' });
-        //     window.localStorage.removeItem('auth');
-        // }
-
         return response;
-        
     }, (error) => {
         let res = error.response;
-        if(res.status === 401 && res.config && !res.config.__isRetryRequest){
+        // console.log('Error response:', res); // Check the value of error.response
+        if(res && res.status === 401 && res.config && !res.config.__isRetryRequest){
             setState(null);
             window.localStorage.removeItem('auth');
             router.push('/login');
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,26 @@ import React from 'react';
 import { useContext } from 'react';
 import { UserContext } from '../context';
 import ParallaxBG from '../components/cards/ParallaxBG';
-function HomePage() {
+import axios from 'axios';
+function HomePage(props) {
 
   const [ state, setState ] = useContext(UserContext);
   return (
     <>
     <ParallaxBG url="/images/default.jpeg"/>
+    <pre>{JSON.stringify(props, null, 4)}</pre>
     </>
   );
+};
+
+export async function getServerSideProps() {
+  const { data } = await axios.get("/posts");
+  // console.log("data =>", data)
+  return {
+    props: {
+      posts: data,
+    },
+  };
 }
 
 export default HomePage;
