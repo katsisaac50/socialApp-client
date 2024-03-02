75441dde6460d5051745beff44205c6490bba953
katsisaac50 <katsisaac50@gmail.com>
2024-03-02 23:57:28 +0100

    clean-up admin index.tsx page

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -2,15 +2,8 @@ import { useContext, useState, useEffect, use } from "react";
 import { useRouter } from "next/router";
 import { UserContext } from "../../context";
 import UserRoute from "../../components/routes/UserRoute";
-import PostForm from "../../components/forms/PostForm";
 import axios from "axios";
 import { toast } from "react-toastify";
-import PostList from "../../components/cards/PostList";
-import People from "../../components/cards/People";
-import CommentForm from "../../components/forms/CommentForm"
-import Link from "next/link";
-import {Modal, Pagination} from "antd";
-import Search from "../../components/search";
 
 const Admin = () => {
   const [state, setState] = useContext(UserContext);
@@ -32,47 +25,6 @@ const Admin = () => {
     try {
       const { data } = await axios.get(`/posts`);
       setPosts(data.posts);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const handleQuillChange = (value) => {
-    // Remove <p> tags from the HTML content
-    const contentWithoutPTags = value.replace(/<\/?p>/gi, "");
-
-    // Set the modified content to state
-    setContent(value);
-  };
-
-  const postSubmit = async () => {
-    try {
-      const {data} = await axios.post(
-        `/create-post`,
-        { content, image },
-        {
-          headers: {
-            Authorization: `Bearer ${state.token}`,
-          },
-        }
-      );
-
-      if (  data.success) {
-        newsFeed();
-        setContent("");
-        setImage({});
-        toast.success(  data.message, {
-          theme: "colored",
-        });
-
-        if (  data.success) {
-          socket.emit("new-post", data.postWithUser);
-        }
-      } else {
-        toast.error(  data.message, {
-          theme: "colored",
-        });
-      }
     } catch (err) {
       console.log(err);
     }
@@ -100,45 +52,6 @@ const Admin = () => {
             <h2 className="">Hello {state.user && state.user.name}</h2>
           </div>
         </div>
-        <div className="row py-3">
-          <div className="col-md-8">
-            <PostForm
-              content={content}
-              handleQuillChange={handleQuillChange}
-              postSubmit={postSubmit}
-              handleImageUpload={handleImageUpload}
-              uploading={uploading}
-              image={image}
-            />
-            <br />
-            <PostList
-              posts={posts}
-              like={like}
-              handleDelete={handleDelete}
-              handleLikes={handleLikes}
-              handleComment={handleComment}
-              removeComment={removeComment}
-            />
-            <Pagination 
-            current={currentPage} 
-            total={(totalPosts/3)*10} 
-            onChange={(value)=>setCurrentPage(value)} 
-            />
-          </div>
-          <div className="col-md-4">
-            <Search />
-            <h3 className="text-center">People you may know</h3>
-            {state && state.user && (
-              <Link href={`/user/following`} legacyBehavior>
-                <a className="h6">{state.user.following.length} Following</a>
-              </Link>
-            )}
-            <People people={state.people} handleFollow={handlefollow} />
-          </div>
-        </div>
-        <Modal visible={visible} onCancel={() => setVisible(false)} title="Comment" footer={null}>
-          <CommentForm addComment={addComment} comment={comment} setComment={setComment}/>
-        </Modal>
       </div>
     </UserRoute>
   );
