445472d0ddf1c45806a9cd40f32faa355eb072d8
katsisaac50 <katsisaac50@gmail.com>
2024-03-19 12:21:42 +0100

    deploy fix

--- next.config.js
+++ next.config.js
@@ -5,15 +5,24 @@
 //     },
 //   };
 
-
-  /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['res.cloudinary.com', 'zos.alipayobjects.com'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+module.exports = {
+  exportPathMap: async function (defaultPathMap) {
+    // Exclude pages with getServerSideProps from export
+    delete defaultPathMap['/post/view/[_id]'];
+    return defaultPathMap;
   },
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+
+//   /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ['res.cloudinary.com', 'zos.alipayobjects.com'],
+//     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+//   },
+// basePath: "/socialApp",
+//   output: "export",  // <=== enables static exports
+//   reactStrictMode: true,
+// };
+
+// module.exports = nextConfig;
