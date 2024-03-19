c9ffc07f3320f11933cd29b2d06b10b6e7d268fd
katsisaac50 <katsisaac50@gmail.com>
2024-03-19 10:59:11 +0100

    enable static page generation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-module.exports = {
-    images: {
-      domains: ['res.cloudinary.com', 'zos.alipayobjects.com'],
-      deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    },
-  };
+// module.exports = {
+//     images: {
+//       domains: ['res.cloudinary.com', 'zos.alipayobjects.com'],
+//       deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+//     },
+//   };
+
+
+  /** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['res.cloudinary.com', 'zos.alipayobjects.com'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
